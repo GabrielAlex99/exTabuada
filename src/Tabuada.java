45f40fdaa3986cea4324bd569e0c79817d2441d0
Gabriel Alexander <galexandersantos975@gmail.com>
2025-09-09 22:44:45 -0300

    adiciona classe

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Tabuada {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int numero = scanner.nextInt();
+
+        int tabuada = 1;
+        while (tabuada <= 10) {
+            System.out.println(numero+"x"+tabuada+" = "+(numero * tabuada++));
+        }
+
+    }
+
 }
